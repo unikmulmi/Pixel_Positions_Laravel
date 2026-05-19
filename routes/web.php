55c2709012281d578b48955a5f5186648220a9ec
unik <unik_mulmi7@hotmail.com>
2026-05-19 12:52:12 +0545

    searchController Completed basic

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class , 'index']);
+
+Route::get('/search' , SearchController::class);
 
 Route::middleware('guest')->group(function() {
     Route::get('/register' , [RegisterUserController::class , 'create']);
